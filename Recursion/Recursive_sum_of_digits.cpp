3ed256d4fe3f6aef07c2bf8a6cd4071b0920b9e8
Ojas-Saraswat <71842941+Ojas-Saraswat@users.noreply.github.com>
2021-02-27 22:49:19 +0530

    Initial Commit

---
+++ Recursion/Recursive_sum_of_digits.cpp
@@ -0,0 +1,62 @@
+//Q..
+//To find sum of digits using recursion
+
+
+#include <bits/stdc++.h>
+typedef long long ll;
+
+using namespace std;
+
+// a very easy ques to understand recursion!!
+//all i want is..get the sum of last digits + sum of remaining digits
+//if the number is 0 or becomes 0..return 0 from that function call
+
+int sum_of_digits(int n) {
+	if (n == 0) {
+		return 0;
+	}
+	else {
+		return sum_of_digits(n / 10) + n % 10;
+	}
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+//for input & output file
+
+
+	int n = 12345;
+	int ans = sum_of_digits(n);
+	cout << ans;
+
+
+	return 0;
+}
+
+
+
+//INPUT
+
+//12345
+
+//OUTPUT
+
+//15
+
+
+//Time complexity
+
+// theta(d)
+//d-> no. of digits
+
+//Space
+
+//theta(d)
