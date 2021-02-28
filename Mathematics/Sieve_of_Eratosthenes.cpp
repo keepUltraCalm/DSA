0bc6cb6e2c6b43d81e2751cbbebf6a40246e88de
Ojas-Saraswat <71842941+Ojas-Saraswat@users.noreply.github.com>
2021-02-28 17:13:51 +0530

    Initial Commit

--- Mathematics/Sieve_of_Eratosthenes.cpp
+++ Mathematics/Sieve_of_Eratosthenes.cpp
@@ -1,6 +1,16 @@
 //Q...
 //Sieve_of_Eratosthenes!!
 
+//we need to print all prime numbers from 0 to that number
+//now the question is how we can we do that??
+
+//logic..
+//ok..so what we do is..first we create a boolean array
+//its size is n+1...now why n+1??
+//because array index stats from 0..so the last indx is n
+//now, we initialize all the index from 2 to n as true..you can leave 0 and 1 index..
+//as we are not going to use these..because they are not prime numbers
+
 #include <bits/stdc++.h>
 typedef long long ll;
 
@@ -14,9 +24,14 @@ void sieve(int n) {
 	for (int i = 2; i * i <= n; i++) {
 		//starting from 2..as 1 is not prime
 		//need to traverse till sqrt n
+		//now why till sqrt n??
+		//because any number..is in the form a x b
+		//now, the lesser of these two numbers will always be <=sqrt of n
+		
 		if (arr[i] == true) {
 			for (int j = 2 * i; j < n + 1; j += i) {
 				//j+=i...to make multiples of i
+				//and then marking them as false because they are not prime
 				arr[j] = false;
 			}
 		}
