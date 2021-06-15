ed69521ed17daf3063a4a8e2a5d6b6a9378a2a39
Ojas-Saraswat <71842941+Ojas-Saraswat@users.noreply.github.com>
2021-06-15 11:46:32 +0530

    Initial Commit

--- Vector_STL/vector_stl.cpp
+++ Vector_STL/vector_stl.cpp
@@ -2,31 +2,32 @@
 #include <vector>
 using namespace std;
 int main() {
-	vector<int> v {10, 20, 30};
+	
+vector<int> v {10, 20, 30};
   
-  cout << v.size() << '\n';
+ cout << v.size() << '\n';
   //prints size of the vector
   
   //vector<int> v = {10,20,30}; works as well
   
    //another way of traversal
-  for(auto it=v.begin(); it!=v.end(); it++){
+ for(auto it=v.begin(); it!=v.end(); it++){
     cout<<*it<<" ";
   }
   cout<<'\n';
   
-	v.push_back(40);
+v.push_back(40);
   //insert 40 at last
   
-	for (auto x : v)
-		cout << x << " ";
-	cout << '\n';
+for (auto x : v)
+cout << x << " ";
+cout << '\n';
   
-	v.pop_back();
+v.pop_back();
   //removes last
   
 	for (auto x : v)
-		cout << x << " ";
+	cout << x << " ";
 	cout << '\n';
   
 	cout << v.front() << " " << v.back() << '\n';
@@ -35,24 +36,27 @@ int main() {
   //can change the front and back..as these two give reference
   
 	for (auto x : v)
-		cout << x << " ";
+	cout << x << " ";
 	cout << '\n';
   
 	v.insert(v.begin(), 2, 100);
   //inserts 100 twice before ist element  
+	
+	v.insert(v.begin(),24);
+	//insert just once
   
 	for (auto x : v)
-		cout << x << " ";
+	cout << x << " ";
 	cout << '\n';
   
 	if (!v.empty())
     //to check vector empty??
-		cout << "Not empty" << '\n';
+	cout << "Not empty" << '\n';
   
   v.clear();
   //removes all elements
 	for (auto x : v)
-		cout << x << " ";
+	cout << x << " ";
 	cout << '\n';
   
 	return 0;
