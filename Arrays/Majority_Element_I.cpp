88f8edd46df1c3dc6c3954905924b60f282ad0f8
Ojas-Saraswat <71842941+Ojas-Saraswat@users.noreply.github.com>
2021-07-09 13:26:55 +0530

    Initial Commit

---
+++ Arrays/Majority_Element_I.cpp
@@ -0,0 +1,93 @@
+/*
+Q. Leetcode : Majority Element I
+
+Find the element which appears more than floor(n/2) times in array
+now, because of n/2 given..we can definitely say only 1 majority element exist
+and it is given in Q that it definitely exists
+
+Brute:
+O(n^2) time
+Space O(1)
+take an element..count its number of occurences
+if >floor(n/2)..this is the ans
+else move ahead
+
+O(nlogn+n)
+O(1)
+2nd approach
+Sort the array
+maintain a max var
+if this max becomes >n/2..this was the element
+
+Better
+Use a hashmap
+If there are no collisions hashmap find is O(1)
+Now, iterate through array..increase frequency if already present
+else
+add that element
+myMap(arr[i])++ ,check this..
+whenever frequency becomes >floor(n/2) you got the ans
+Time: O(n)
+Space : O(n)
+*/
+
+/*
+Optimal sol:
+
+Moore's voting algorithm
+
+See...we are 100% sure majority element is present
+so even on using all the other numbers to exhaust this element
+it will still be left
+1 1 1  3 4   1 will still be left
+This is the intution..
+if it occurs between two different..then it will occur later again
+if it occurs together..imagine..then count will never become 0
+if it is mixture of both..we will see it again
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define mod 1000000007;
+
+int main() {
+
+    cout << fixed;
+    cout << setprecision(2);
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int arr[] = {7, 7, 5, 5, 7, 5, 1, 5, 7, 5, 5, 7, 7, 5, 5, 5, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int count = 1;
+    int ans = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (count == 0)
+            ans = arr[i];
+        if (arr[i] == ans)
+            count++;
+        else
+            count--;
+
+    }
+    cout << ans;
+
+    return 0;
+}
+
+/*
+O/P
+5
+
+Time: O(n)
+Space: O(1)
+*/
