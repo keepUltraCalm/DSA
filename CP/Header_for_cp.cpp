9251cd8b88265e1e4c0c0c3138cddab9dc1bfedb
letsBuildSomethingCool <71842941+letsBuildSomethingCool@users.noreply.github.com>
2021-08-07 18:50:41 +0530

    Initial Commit

--- CP/Header_for_cp.cpp
+++ CP/Header_for_cp.cpp
@@ -4,6 +4,44 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
+/*
+int binary_to_decimal(string &str)
+{
+	int n = stoi(str);
+	int pow = 1;
+	int ans = 0;
+	while (n > 0)
+	{
+		ans += (n % 10) * pow;
+		pow *= 2;
+		n /= 10;
+	}
+	return ans;
+}
+*/
+void binary_to_decimal(string num){
+	int t = stoi(num, 0, 2);
+	cout << t;
+}
+/*
+string decimal_to_binary(int n)
+{
+	string str;
+	while (n > 0)
+	{
+		string s = to_string(n % 2);
+		str += s;
+		n /= 2;
+	}
+	return str;
+}
+*/
+void decimal_to_binary(int n){
+	while (n > 0){
+		cout<<n % 2;
+		n /= 2;
+	}
+}
 int main() {
 
 	cout << fixed;
@@ -21,7 +59,6 @@ int main() {
 	ull n;
 	cin >> n;
 	cout << n;
-
 
 	return 0;
 }
