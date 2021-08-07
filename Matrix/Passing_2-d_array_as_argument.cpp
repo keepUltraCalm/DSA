23f1dd740372f4af5128ccf748b6b7814bd68cb7
letsBuildSomethingCool <71842941+letsBuildSomethingCool@users.noreply.github.com>
2021-08-07 18:43:43 +0530

    Initial Commit

---
+++ Matrix/Passing_2-d_array_as_argument.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void print(int *arr[], int m, int n) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++)
+			cout << arr[i][j] << " ";
+		cout << '\n';
+	}
+}
+
+int main() {
+	int m = 3, n = 3;
+	int *arr[m];
+	for (int i = 0; i < m; i++) {
+		arr[i] = new int[n];
+		for (int j = 0; j < n; j++) {
+			arr[i][j] = j;
+			print(arr, m, n);
+			return 0;
+		}
+		//multi dim array c++
