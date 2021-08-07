605bc6e7bbd04f6e25903ce81a71756084e5d38b
letsBuildSomethingCool <71842941+letsBuildSomethingCool@users.noreply.github.com>
2021-08-07 15:56:33 +0530

    Initial Commit

---
+++ CP/Header_for_cp.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007;
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main() {
+
+	cout << fixed;
+	cout << setprecision(2);
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ull n;
+	cin >> n;
+	cout << n;
+
+
+	return 0;
+}
